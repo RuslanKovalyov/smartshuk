cc831b56c5fff39618c48c90253aaaceafe26427
Ruslan <kovalyovruslan@gmail.com>
2023-08-11 17:09:10 +0300

    robot txt

--- smartshuk/urls.py
+++ smartshuk/urls.py
@@ -19,3 +19,11 @@ from .settings import DEV_MODE
 if DEV_MODE:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^robots\.txt$', serve, {'document_root': settings.BASE_DIR, 'path': "/robots.txt"}),
+]
